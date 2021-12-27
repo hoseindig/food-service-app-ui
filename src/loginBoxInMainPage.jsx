b829ce1beaf7e4855bee3f33968e34075159f420
hoseindig <hosein_dig@yahoo.com>
2021-12-27 11:44:06 +0330

    add bootstrap 5 . add font iran sans

--- src/loginBoxInMainPage.jsx
+++ src/loginBoxInMainPage.jsx
@@ -2,12 +2,21 @@ const LoginBoxInMainPage = () => {
   return (
     <div className="LoginBox">
       <i className="fa fa-user-o" aria-hidden="true"></i>
-      <i className="fa fa-heart-o" aria-hidden="true"></i>
+      {/* <i className="fa fa-heart-o" aria-hidden="true"></i> */}
 
-      <i className="fa fa-shopping-basket"></i>
+      <div className=" fa fa-heart-o position-relative">
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          0<span className="visually-hidden">unread messages</span>
+        </span>
+      </div>
+
+      <div className=" fa fa-shopping-basket position-relative">
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          0<span className="visually-hidden">unread messages</span>
+        </span>
+      </div>
 
       <i className="fa fa-search" aria-hidden="true"></i>
-     
     </div>
   );
 };
--- src/orderByTell.jsx
+++ src/orderByTell.jsx
@@ -2,7 +2,8 @@ const OrderByTell = () => {
   return (
     <div className="OrderByTell">
       <span>برای سفارش تلقنی تماس بگیرید</span>
-      <p>
+      {/* onClick={window.open('tel:900300400')} */}
+      <p >
         02166066366 <i className="fa fa-phone" aria-hidden="true"></i> 
       </p>
     </div>
