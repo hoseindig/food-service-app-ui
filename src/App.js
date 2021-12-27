ba8b78be7fc8aa13bcba7b211965bed50865b8a4
hoseindig <hosein_dig@yahoo.com>
2021-12-27 10:28:38 +0330

    login box

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 // import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import Navbar from "./navbar";
 import QuickMenu from './quickMenu'; 
 import MainBox from './mainBox';
+import LoginBoxInMainPage from './loginBoxInMainPage';
 function App() {
   return (
     <div className="App">
       <Navbar />
       <QuickMenu />
+      <LoginBoxInMainPage/>
       <MainBox className="header"/>
     </div>
   );
--- src/quickMenu.jsx
+++ src/quickMenu.jsx
@@ -1,18 +1,33 @@
-import React  from 'react';
-
+import React from "react";
 
 const QuickMenu = () => {
-    return ( <React.Fragment>
-        <ul className="quickMenu">
-            <li><a to=""> پیتزا</a><i class="fas fa-pizza-slice"></i></li>
-            <li><a to="">همبرگر</a></li>
-            <li><a to="">ساندویچ</a></li>
-            <li><a to="">سالاد و سایر</a></li>
-            <li><a to="">سیبزمنی تنوری</a></li>
-            <li><a to="">نوشیدنی</a></li>
-            <li><a to="">دسر</a></li>
-        </ul>
-    </React.Fragment> );
-}
- 
+  return (
+    <React.Fragment>
+      <ul className="quickMenu">
+        <li>
+          <a to=""> پیتزا</a>
+        </li>
+        <li>
+          <a to="">همبرگر</a>
+        </li>
+        <li>
+          <a to="">ساندویچ</a>
+        </li>
+        <li>
+          <a to="">سالاد و سایر</a>
+        </li>
+        <li>
+          <a to="">سیبزمنی تنوری</a>
+        </li>
+        <li>
+          <a to="">نوشیدنی</a>
+        </li>
+        <li>
+          <a to="">دسر</a>
+        </li>
+      </ul>
+    </React.Fragment>
+  );
+};
+
 export default QuickMenu;
