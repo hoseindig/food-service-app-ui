a6bbdce9b33a7d06a66bcac0ea211f481a12b7c4
hoseindig <hosein_dig@yahoo.com>
2022-01-24 16:33:32 +0330

    toastify added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 // import logo from './logo.svg';
 
 import "./App.scss";
+import { ToastContainer } from "react-toastify";
 
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import "react-toastify/dist/ReactToastify.css";
 
 import Main from "./main";
 // import Products from "./component/products";
 function App() {
   return (
     <div className="App">
+      <ToastContainer position="bottom-right"/>
       <Main />
     </div>
   );
