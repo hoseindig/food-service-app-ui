d0160c45a1ad1d6eece8608859d8e2f9e5f0f2a6
hoseindig <hosein_dig@yahoo.com>
2022-01-31 18:47:12 +0330

    service edit for mongo

--- service/socket-service/index.js
+++ service/socket-service/index.js
@@ -1,3 +1,7 @@
+//1400-11-10 hosein Sheykhi
+const uuidv4 = require("uuid").v4;
+const MongoClient = require("mongodb").MongoClient;
+
 const httpServer = require("http").createServer();
 const io = require("socket.io")(httpServer, {
   cors: {
@@ -23,12 +27,13 @@ const dbUsers = [
     password: "1",
   },
 ];
+let orders = [];
 
 io.on("connection", (socket) => {
   const userConected = socket.handshake.auth;
   const users = [];
 
-  console.log("io.on connection", socket.username);
+  console.log("io.on connection", socket.username, socket.id);
 
   //if user is admin veryfy
   const resAdminFind = dbUsers.find(
@@ -81,6 +86,131 @@ io.on("connection", (socket) => {
     });
   });
 
+  ///////////////////////////////////////
+  function getDateTimeNow() {
+    const today = new Date();
+    const dateTime =
+      today.getFullYear() +
+      "-" +
+      (today.getMonth() + 1) +
+      "-" +
+      today.getDate() +
+      " " +
+      today.getHours() +
+      ":" +
+      today.getMinutes() +
+      ":" +
+      today.getSeconds();
+    return dateTime;
+  }
+
+  async function getOrderFromDb(tackingNumber) {
+    var url = "mongodb://localhost:27017/";
+    const query = { id: 1643358677550 };
+    let myPromise = new Promise(function (resolve, reject) {
+      // resolve("I love You !!");
+      MongoClient.connect(url, function (err, db) {
+        if (err) throw err;
+        var dbo = db.db("test01");
+        dbo
+          .collection("orders")
+          .find({ id: tackingNumber })
+          // .find({}, { id: 1643358677550 })
+          // .findOne(query)
+          .toArray(function (err, result) {
+            if (err) throw err;
+            console.log("getOrderFromDb", result);
+            resolve(result);
+            db.close();
+          });
+      });
+    });
+    return await myPromise;
+  }
+
+  function findAndUpdateOrder(order, to, socket) {
+    const index = orders.findIndex((i) => i.id === order.id);
+    if (index !== 1) {
+      orders[index] = order;
+      socket.to(to).emit("orders", {
+        orders,
+        from: socket.id,
+      });
+    } else console.log("findAndUpdateOeder not found");
+  }
+
+  function sendOrder(to) {
+    orders.forEach((order) => {
+      console.log("sendOrder userID", order.userID, order);
+      socket.to(to).emit("orders", order); //.to(order.userID)
+    });
+  }
+  ///////////////////////////////////////
+  //                orders             //
+
+  ///////////////////////////////////////
+  socket.on("test", () => {
+    console.log("socket.emit test");
+    socket.emit("test", 1);
+  });
+
+  //new order =>1)save order 2) refer order
+  socket.on("newOrder", ({ orders, to }) => {
+    console.log("newOrder", orders, to, socket.id);
+    const orderObject = {
+      orders,
+      from: socket.id,
+      to: to,
+      id: uuidv4(),
+      time: getDateTimeNow(),
+      type: 1,
+      state: 1,
+    };
+    orders.push(orderObject);
+    socket.to(to).emit("newOrder", { orderObject });
+  });
+  // getOrderState 1)get from data base 2)add to orders 3)send to client
+  socket.on("getOrderState", ({ tackingNumber, to }) => {
+    console.log("getOrderState", tackingNumber, to, socket.id);
+
+    getOrderFromDb(tackingNumber)
+      .then((res) => {
+        console.log("#getOrderState emit", res);
+
+        const shopList = res[0].shopList; //get new orders from db and send to kichecn
+        const trackNumber = res[0].id; //get new orders from db and send to kichecn
+
+        orders.push({
+          orders: shopList,
+          trackNumber,
+          userID: socket.id,
+          AdminID: to,
+          state: 1,
+        });
+        // sendOrder(socket.id);
+        orders.forEach((order) => {
+          console.log("sendOrder userID", order.userID, order);
+          //   socket.emit("orders", order);
+          socket.to(socket.id).emit("test");
+          socket.to(socket.id).emit("orders", { order }); //.to(order.userID)
+        });
+      })
+      .catch((err) => {
+        console.log("#getOrderState err", err);
+      });
+  });
+  //get ordders
+  socket.on("getOrders", () => {
+    console.log("getOrders");
+    sendOrder(socket.id);
+  });
+  //change order state 1)find order 2)update order 3)send to user
+  socket.on("setOrderState", ({ order, to }) => {
+    console.log("setOrderState", order, to, socket.id);
+    findAndUpdateOrder(order, to, socket.id);
+  });
+
+  ///////////////////////////////////////
   // notify users upon disconnection
   socket.on("disconnect", () => {
     socket.broadcast.emit("user disconnected", socket.id);
